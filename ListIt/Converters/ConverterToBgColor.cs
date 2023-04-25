451a44bb700322ac58b53a19e6df4c4f58589ea1
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-04-25 12:00:46 -0300

    feat: add converters background and text

---
+++ ListIt/Converters/ConverterToBgColor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ListIt.Converters
+{
+    internal class ConverterToBgColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = $"{value}";
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (text == "Mercado")
+                {
+                    return "#FDFFE1";
+                }
+                else if (text == "Eletronicos")
+                {
+                    return "#CEEADC";
+                }
+                else if (text == "Oficina")
+                {
+                    return "#FFCEA1";
+                }
+                else if (text == "Farmácia")
+                {
+                    return "#FFDACF";
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ ListIt/Converters/ConverterToTextColor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListIt.Converters
+{
+    internal class ConverterToTextColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = $"{value}";
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (text == "Mercado")
+                {
+                    return "#443F3F";
+                }
+                else if (text == "Eletronicos")
+                {
+                    return "#FFFFFF";
+                }
+                else if (text == "Oficina")
+                {
+                    return "#443F3F";
+                }
+                else if (text == "Farmácia")
+                {
+                    return "#443F3F";
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
