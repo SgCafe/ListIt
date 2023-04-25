c26c706c933752085cba66b0b95a7bcb23c150c0
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-04-25 12:06:41 -0300

    feat: add models

---
+++ ListIt/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListIt.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public TypeCategory TypeCategory { get; set; }
+        public int? Quantity { get; set; }
+        public double? Value { get; set; }
+    }
+
+    public class TypeCategory
+    {
+        public string Category { get; set; }
+        public string Image { get; set; }
+    }
+}
