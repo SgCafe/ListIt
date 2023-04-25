d851ddd643261f657d883d75157a97b4419d4e0a
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-04-25 11:48:05 -0300

    feat: add shell and splashscreen in project

--- ListIt/Views/ProductPage.xaml.cs
+++ ListIt/Views/ProductPage.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace ListIt
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListIt.Views
 {
-    public partial class MainPage : Shell
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductPage : ContentPage
     {
-        public MainPage()
+        public ProductPage()
         {
             InitializeComponent();
         }
