1a00b8542373d8ef6c471845fc55f8c7a14adbc4
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-04-26 20:38:59 -0300

    feat: add viewmodels

--- ListIt/Viewmodels/AddProductViewModel.cs
+++ ListIt/Viewmodels/AddProductViewModel.cs
@@ -1,10 +1,129 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ListIt.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ListIt.Viewmodels
 {
-    internal class AddProductViewModel
+    internal class AddProductViewModel : BaseViewmodel
     {
+        #region properties
+
+        private ObservableCollection<Product> _categoriesList;
+        public ObservableCollection<Product> CategoriesList
+        {
+            get => _categoriesList;
+            set => SetProperty(ref _categoriesList, value);
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set => SetProperty(ref _nome, value);
+        }
+
+        private Product _categoria;
+
+        public Product Categoria
+        {
+            get => _categoria;
+            set => SetProperty(ref _categoria, value).ToString();
+        }
+
+
+        private int? _quantidade;
+        public int? Quantidade
+        {
+
+            get { return _quantidade; }
+            set => SetProperty(ref _quantidade, value);
+        }
+
+        private double? _valor;
+        public double? Valor
+        {
+            get { return _valor; }
+            set => SetProperty(ref _valor, value);
+        }
+        #endregion
+
+        #region Constructor
+        public AddProductViewModel()
+        {
+            CategoryList();
+            BackProductPageCommand = new Command(ExecuteBackProductPageCommand);
+            SaveProductCommand = new Command(ExecuteSaveProductCommand);
+        }
+        #endregion
+
+        #region commands
+        public ICommand BackProductPageCommand { get; set; }
+        public ICommand SaveProductCommand { get; set; }
+        #endregion
+
+
+        #region methods
+
+        private void CategoryList()
+        {
+            CategoriesList = new ObservableCollection<Product>()
+            {
+                new Product
+                {
+                    Category = "Mercado",
+                    Image = "ps_cart.png"
+                },
+                new Product
+                {
+                    Category = "Eletronicos",
+                    Image = "computer_simbolo.png"
+                },
+                new Product
+                {
+                    Category = "Oficina",
+                    Image = "ph_gear.png"
+                },
+                new Product
+                {
+                    Category = "Farmácia",
+                    Image = "mdi_health.png"
+                },
+            };
+
+        }
+
+
+        private async void ExecuteSaveProductCommand()
+        {
+            var item = new Product
+            {
+                Name = Nome,
+                Quantity = Quantidade,
+                Value = Valor,
+                Category = Categoria.Category,
+                Image = Categoria.Image,
+
+            };
+
+
+            //await App.SQLiteDb.AddProductAsync(newItem);
+            
+            Nome = string.Empty;
+            Quantidade = null;
+            Valor = null;
+            Categoria = null;
+
+            await Shell.Current.GoToAsync("//ProductPageShell");
+        }
+
+
+
+        private async void ExecuteBackProductPageCommand()
+        {
+            await Shell.Current.GoToAsync("//ProductPageShell");
+        }
+
+        #endregion
     }
 }
--- ListIt/Viewmodels/ProductViewmodel.cs
+++ ListIt/Viewmodels/ProductViewmodel.cs
@@ -1,10 +1,185 @@
-﻿using System;
+﻿using ListIt.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ListIt.Viewmodels
 {
-    internal class ProductViewmodel
+    public class ProductViewmodel : BaseViewmodel
     {
+        #region  properties
+        private Product product;
+
+        public Product ProductTlg
+        {
+            get => product;
+            set => SetProperty(ref product, value);
+        }
+
+        private string _selectedProduct;
+        public string SelectedProduct
+        {
+            get => _selectedProduct;
+            set => SetProperty(ref _selectedProduct, value);
+        }
+
+
+        private ObservableCollection<Product> _itemsList;
+        public ObservableCollection<Product> ItemsList
+        {
+            get => _itemsList;
+            set => SetProperty(ref _itemsList, value);
+        }
+
+        private bool _isBusy = false;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+        #endregion
+
+        #region constructor
+        public ProductViewmodel()
+        {
+            PopulateList();
+            NavAddProductCommand = new Command(() => ExecuteNavAddPageCommand());
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            DeleteItemCommand = new Command<Product>(ExecuteDeleteItemCommand);
+            UpdateItemCommand = new Command<Product>((product) => ExecuteUpdateItemCommand(product));
+        }
+        #endregion
+
+
+        #region commands
+        public ICommand LoadItemsCommand { get; }
+        public ICommand DeleteItemCommand { get; set; }
+        public ICommand UpdateItemCommand { get; }
+        public ICommand NavAddProductCommand { get; set; }
+        #endregion
+
+        #region methods
+        public int ItemCount => ItemsList.Count;
+
+        public double TotalValue => (double)ItemsList.Sum(item => item.Value);
+        private void PopulateList()
+        {
+            ItemsList = new ObservableCollection<Product>()
+            {
+                //new Product
+                //{
+                //    Name = "teste",
+                //    Category = "Mercado",
+                //    Quantity= 1,
+                //    Value = 200,
+                //    Image = "ph_gear.png"
+                //},
+                //new Product
+                //{
+                //    Name = "Roda",
+                //    Category = "Oficina",
+                //    Quantity= 2,
+                //    Value = 2400,
+                //    Image = "ph_gear.png"
+                //}
+
+            };
+
+            ItemsList.CollectionChanged += OnItemsListChanged;
+        }
+
+        private void OnItemsListChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(ItemCount));
+            OnPropertyChanged(nameof(TotalValue));
+
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            try
+            {
+                if (IsBusy)
+                {
+                    return;
+                }
+                IsBusy = true;
+
+                ItemsList.Clear();
+
+                //var items = await App.SQLiteDb.GetAllProductsAsync();
+                //foreach (var item in items)
+                //{
+                //    ItemsList.Add(item);
+                //}
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async void ExecuteDeleteItemCommand(Product product)
+        {
+            if (product == null)
+            {
+                return;
+            }
+
+            //await App.SQLiteDb.DeleteProductAsync(product.Id);
+            await ExecuteLoadItemsCommand();
+        }
+
+        private async void ExecuteUpdateItemCommand(Product product)
+        {
+            try
+            {
+                if (IsBusy) { return; }
+                IsBusy = true;
+
+                //var existingitem = await App.SQLiteDb.GetProductAsync(product.Id);
+
+                await Shell.Current.GoToAsync($"//AddProductShell?parametro_id={product}");
+                //await App.SQLiteDb.UpdateProductAsync(product);
+
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok!");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+            //var existingitem = await App.SQLiteDb.GetProductAsync(id.Id);
+            //if (existingitem != null)
+            //{
+            //    existingitem.Name = product.Name;
+            //    existingitem.Value = product.Value;
+            //    existingitem.Quantity = product.Quantity;
+            //    existingitem.Category = product.Category;
+            //    existingitem.Image = product.Image;
+            //    await Shell.Current.GoToAsync("//AddProductShell");
+            //    await App.SQLiteDb.UpdateProductAsync(id);
+            //}
+        }
+
+        private async void ExecuteNavAddPageCommand()
+        {
+            await Shell.Current.GoToAsync("//AddProductShell");
+        }
+        #endregion
     }
 }
